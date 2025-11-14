214d6ec3640e7aa8ecc95d73de5ffb0314b9385c
SMAIL IKHLEF <smailikhlef@gmail.com>
2025-11-14 12:40:01 -0500

    Exercice 5 : classe abstraite Forme, Cercle et Rectangle

---
+++ EXO5_formes.dart
@@ -0,0 +1,37 @@
+import 'dart.math';
+
+abstract class Forme {
+  double calculerAire();
+  void afficherMessage() {
+    print('Calcul de l aire...');
+  }
+}
+
+class Cercle extends Forme {
+  final double rayon;
+
+  Cercle(this.rayon);
+  @override
+  double calculerAire() {
+    return pi * rayon * rayon;
+  }
+}
+
+class Rectangle extends Forme {
+  final double longueur;
+  final double largeur;
+  Rectangle(this.longueur, this.largeur);
+
+  @override
+  double calculerAire() {
+    return longueur * largeur;
+  }
+}
+
+void main() {
+  var cercle = Cercle(5);
+  var rectangle = Rectangle(4, 3);
+
+  cercle.afficheMessage();
+  print('Aire du cercle : ${cercle.calculerAire()}');
+}
